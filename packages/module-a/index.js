c27719ad2d23a5d6f06b9e6d2c253eee4bd008c4
luzhiyong <luzhiyong@tuzhanai.com>
2019-06-05 10:46:16 +0800

    'init'

---
+++ packages/module-a/index.js
@@ -0,0 +1,10 @@
+/*
+ * [page a]
+ * @Created by L on 2019-05-30 16:49:07
+ * @Last Modified by: L
+ * @Last Modified time: 2019-05-30 16:55:48
+ */
+export default {
+  a: '1',
+  b: '2',
+};
---
+++ packages/module-b/index.js
@@ -0,0 +1,13 @@
+/*
+ * [page b]
+ * @Created by L on 2019-05-30 16:52:27
+ * @Last Modified by: L
+ * @Last Modified time: 2019-05-30 16:58:21
+ */
+import A from '@my-package/module-a';
+
+const B = 'I am module B';
+
+console.log(A, B);
+
+export { A, B };
